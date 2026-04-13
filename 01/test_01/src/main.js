5ec8dd8c215dfbc627124ac8d3c735abefd8874c
suzuki119 <suzukiyutaro119@gmail.com>
2026-04-13 12:21:41 +0900

    first commit

---
+++ 01/test_01/src/main.js
@@ -0,0 +1,83 @@
+import './style.css';
+import * as THREE from 'three';
+
+// 1. シーンの作成
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#336699'); // 追加
+
+// 2. カメラの作成
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+
+// 3. レンダラーの作成
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+// 4. ジオメトリ（形状）の作成： 幅1, 高さ1, 奥行き1の立方体
+const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+// 5. マテリアル（質感）の作成： 光がなくても立体感がわかるノーマルマテリアル
+const material = new THREE.MeshNormalMaterial();
+
+// 6. メッシュで組み合わせる（形状 + 質感）
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
+const keys = {};
+
+document.addEventListener('keydown', (e) => {
+  keys[e.code] = true;
+});
+
+document.addEventListener('keyup', (e) => {
+  keys[e.code] = false;
+});
+
+// 7. アニメーション（毎フレーム更新）
+let prevTime = performance.now();
+
+const update = () => {
+  window.requestAnimationFrame(update);
+
+
+
+  const speed = 0.1; // 1秒あたりの移動量
+
+  if (keys['KeyW'] || keys['ArrowUp']) {
+    cube.position.y += speed;
+  }
+  if (keys['KeyS'] || keys['ArrowDown']) {
+    cube.position.y -= speed;
+  }
+  if (keys['KeyA'] || keys['ArrowLeft']) {
+    cube.position.x -= speed;
+  }
+  if (keys['KeyD'] || keys['ArrowRight']) {
+    cube.position.x += speed;
+  }
+
+  // 回転（そのままでOK）
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+update();
+
+// 8. ウィンドウのリサイズに対応
+const onWindowResize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+};
+
+window.addEventListener('resize', onWindowResize);
